df7f539a512a3128bc13478c453c296c9e439f75
myself <saiharshakosaraju@Sais-MacBook-Pro.local>
2023-06-24 11:04:30 +0530

    intial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div className="jumbotron jumbotron-fluid d-flex align-items-center justify-content-center" style={{ minHeight: '65vh' }}>
+      <div className="container">
+        <h2 className="text-dark text-center">This is a Single Page Application made by using React.</h2>
+        <br />
+        <br />
+        <h4 className="text-dark text-center">Made with Love by</h4>
+        <h6 className="text-dark text-center">-Sai Harsha</h6>
+      </div>
+    </div>
+  );
+};
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer className="bg-dark text-light py-3" style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+      <div className="container d-flex justify-content-center">
+        <p className="text-center">
+          Copyright&copy; Kosaraju Sai Harsha
+        </p>
+      </div>
+    </footer>
+  );
+};
